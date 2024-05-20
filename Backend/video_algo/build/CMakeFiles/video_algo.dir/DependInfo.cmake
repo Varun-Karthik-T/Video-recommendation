51e22fc93acfdae687243c7dd69b63f0467bb544
Rishi-kumar18 <rk18102004@gmail.com>
2024-05-20 19:36:27 +0530

    something fe

--- Backend/video_algo/build/CMakeFiles/video_algo.dir/DependInfo.cmake
+++ Backend/video_algo/build/CMakeFiles/video_algo.dir/DependInfo.cmake
@@ -8,11 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/e/Code/C++ webdev/Video-algo/Backend/video_algo/main.cpp" "CMakeFiles/video_algo.dir/main.cpp.o" "gcc" "CMakeFiles/video_algo.dir/main.cpp.o.d"
+  "/mnt/c/Users/rk181/OneDrive/Documents/GitHub/Video-recommendation/Backend/video_algo/main.cpp" "CMakeFiles/video_algo.dir/main.cpp.o" "gcc" "CMakeFiles/video_algo.dir/main.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- Backend/video_algo/build/cmake_install.cmake
+++ Backend/video_algo/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/e/Code/C++ webdev/Video-algo/Backend/video_algo
+# Install script for directory: /mnt/c/Users/rk181/OneDrive/Documents/GitHub/Video-recommendation/Backend/video_algo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/e/Code/C++ webdev/Video-algo/Backend/video_algo/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/rk181/OneDrive/Documents/GitHub/Video-recommendation/Backend/video_algo/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
