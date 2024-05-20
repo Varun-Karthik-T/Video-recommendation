b5815da2a97e1ac28ff508d7e1628ab8226c1321
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-05-20 18:57:44 +0530

    crow config done

---
+++ Backend/video_algo/main.cpp
@@ -0,0 +1,14 @@
+#include "crow.h"
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello, Crow!";
+    });
+
+    app.port(18080).multithreaded().run();
+
+    return 0;
+}
